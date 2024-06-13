72d7ad8fcf067af007b6037121b571fc8ad066e1
Antonella <adparra2@utpl.edu.ec>
2024-06-13 15:49:32 -0500

    Cambios

--- EjemploHerencia02/HerenciaUno/src/paquete2/EstudianteDistancia.java
+++ EjemploHerencia02/HerenciaUno/src/paquete2/EstudianteDistancia.java
@@ -1,40 +1,41 @@
 package paquete2;
 
-public class EstudianteDistancia extends Estudiante{
-    
+public class EstudianteDistancia extends Estudiante {
+
+    // Presenta un error ya que estudiante cuenta con un constructor
+    // y al no tener un constructor que reciba los parametros marca error
     private int numeroAsignaturas;
     private double costoAsignatura;
     private double matriculaDistancia;
-    
-    
+
     // 2.  Método establecerNumeroAsginaturas(numero: Real)
-    public void establecerNumeroAsginaturas(int numero){
+    public void establecerNumeroAsginaturas(int numero) {
         numeroAsignaturas = numero;
     }
-    
+
     // 3.  Método establecerCostoAsignatura(valor: Real)
-    public void establecerCostoAsignatura(double valor){
+    public void establecerCostoAsignatura(double valor) {
         costoAsignatura = valor;
     }
 
     // 4.  Método calcularMatriculaDistancia()
-    public void calcularMatriculaDistancia(){
+    public void calcularMatriculaDistancia() {
         matriculaDistancia = numeroAsignaturas * costoAsignatura;
     }
 
     //  Métodos obtener para los datos o atributos de la clase
     // 5. Método obtenerNumeroAsignaturas() : Entero
-    public int obtenerNumeroAsignaturas(){
-        return numeroAsignaturas; 
+    public int obtenerNumeroAsignaturas() {
+        return numeroAsignaturas;
     }
 
     // 6. Método obtenerCostoAsignatura() : Real
-    public double obtenerCostoAsignatura(){
+    public double obtenerCostoAsignatura() {
         return costoAsignatura;
     }
 
     // 7. Método obtenerMatriculaDistancia() : Real
-    public double obtenerMatriculaDistancia(){
+    public double obtenerMatriculaDistancia() {
         return matriculaDistancia;
     }
 
--- EjemploHerencia02/HerenciaUno/src/paquete2/EstudiantePresencial.java
+++ EjemploHerencia02/HerenciaUno/src/paquete2/EstudiantePresencial.java
@@ -1,8 +1,9 @@
-
 package paquete2;
 
-public class EstudiantePresencial extends Estudiante{
-    
+public class EstudiantePresencial extends Estudiante {
+
+    // Presenta un error ya que estudiante cuenta con un constructor
+    // y al no tener un constructor que reciba los parametros marca error
     /*1.  Declarar
         numeroCreditos: Entero 
         costoCredito: Real 
@@ -10,37 +11,37 @@ public class EstudiantePresencial extends Estudiante{
     private int numeroCreditos;
     private double costoCredito;
     private double matriculaPresencial;
-    
+
     //  Métodos establecer y calcular para los datos o atributos de la clase
     // 2.  Método establecerNumeroCreditos(numero: Real)
-    public void establecerNumeroCreditos(int numero){
+    public void establecerNumeroCreditos(int numero) {
         numeroCreditos = numero;
     }
-    
+
     // 3.  Método establecerCostoCredito(valor: Real)
-    public void establecerCostoCredito(double valor){
+    public void establecerCostoCredito(double valor) {
         costoCredito = valor;
     }
 
     // 4.  Método calcularMatriculaPresencial()
-    public void calcularMatriculaPresencial(){
+    public void calcularMatriculaPresencial() {
         matriculaPresencial = numeroCreditos * costoCredito;
     }
 
     //  Métodos obtener para los datos o atributos de la clase
     // 5. Método obtenerNumeroCreditos() : Entero
-    public int obtenerNumeroCreditos(){
-        return numeroCreditos; 
+    public int obtenerNumeroCreditos() {
+        return numeroCreditos;
     }
 
     // 6. Método obtenerCostoCredito() : Real
-    public double obtenerCostoCredito(){
+    public double obtenerCostoCredito() {
         return costoCredito;
     }
 
     // 7. Método obtenerMatriculaPresencial() : Real
-    public double obtenerMatriculaPresencial(){
+    public double obtenerMatriculaPresencial() {
         return matriculaPresencial;
     }
-    
+
 }
