b05441fd67f10d62a6c1488561794f73308d816d
Antonella <adparra2@utpl.edu.ec>
2024-06-13 16:09:34 -0500

    Cambios

--- EjemploHerencia02/HerenciaUno/src/paquete3/EstudiantePresencial.java
+++ EjemploHerencia02/HerenciaUno/src/paquete3/EstudiantePresencial.java
@@ -1,47 +1,68 @@
-
 package paquete3;
 
-public class EstudiantePresencial extends Estudiante{
-    
+public class EstudiantePresencial extends Estudiante {
+
     int numeroCreditos;
     double costoCredito;
     double matriculaPresencial;
-    
-    public EstudiantePresencial(String n, String ap, String iden, int e){
+
+    public EstudiantePresencial(String n, String ap, String iden, int e) {
         // public Estudiante(String n, String a, String iden, int ed){
         super(n, ap, iden, e); // cumplir con el constructor de la superClase
     }
-    
+
     //  Métodos establecer y calcular para los datos o atributos de la clase
     // 2.  Método establecerNumeroCreditos(numero: Real)
-    public void establecerNumeroCreditos(int numero){
+    public void establecerNumeroCreditos(int numero) {
         numeroCreditos = numero;
     }
-    
+
     // 3.  Método establecerCostoCredito(valor: Real)
-    public void establecerCostoCredito(double valor){
+    public void establecerCostoCredito(double valor) {
         costoCredito = valor;
     }
 
     // 4.  Método calcularMatriculaPresencial()
-    public void calcularMatriculaPresencial(){
+    public void calcularMatriculaPresencial() {
         matriculaPresencial = numeroCreditos * costoCredito;
     }
 
     //  Métodos obtener para los datos o atributos de la clase
     // 5. Método obtenerNumeroCreditos() : Entero
-    public int obtenerNumeroCreditos(){
-        return numeroCreditos; 
+    public int obtenerNumeroCreditos() {
+        return numeroCreditos;
     }
 
     // 6. Método obtenerCostoCredito() : Real
-    public double obtenerCostoCredito(){
+    public double obtenerCostoCredito() {
         return costoCredito;
     }
 
     // 7. Método obtenerMatriculaPresencial() : Real
-    public double obtenerMatriculaPresencial(){
+    public double obtenerMatriculaPresencial() {
         return matriculaPresencial;
     }
-    
+
+    /*
+    @Override
+    public String toString() {
+        String cadena = String.format("%s\n"
+                + "Numero de creditos:%d\n"
+                + "Costo de creditos: %.2f\n"
+                + "Valor matricula: %.2f\n",
+                super.toString(),
+                numeroCreditos, costoCredito, matriculaPresencial);
+        return cadena;
+        // Para poder imprimir el metodo toString de la super clase lo llamamos a
+        // super punto to string
+    }
+     */
+    @Override
+    public String toString() {
+        String cadena = String.format("Apellido: %s\n"
+                + "Identificacion: %s\n"
+                + "Valor matricula: %.2f\n",
+                apellidosEstudiante, identificacionEstudiante, matriculaPresencial);
+        return cadena;
+    }
 }
